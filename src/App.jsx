1a44693ebe51148e7a4b54a8a101e11b3e9627a5
iamsid0908 <siddharthchandrakar007@gmail.com>
2024-04-14 10:19:34 +0530

    icons-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  console.log(import.meta.env.PORT);
   return (
     <>
       <div>
--- src/Components/Navbar/Navmobile.jsx
+++ src/Components/Navbar/Navmobile.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./Navbar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 
 function Navmobile() {
   return (
     <div>
       <div className="navmobile">
-        <a>Home</a>
-        <a>About us</a>
-        <a>Addmission</a>
-        <a>Contact us</a>
+        <a>
+          <HomeIcon />
+        </a>
+        <a>
+          <PeopleAltIcon />
+        </a>
+        <a>
+          <AddBoxIcon />
+        </a>
+        <a>
+          <PermContactCalendarIcon />
+        </a>
       </div>
     </div>
   );
