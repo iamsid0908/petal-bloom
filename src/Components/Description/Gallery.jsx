ae3feb0da8675806b4dd0975f8c42f7ea1207050
iamsid0908 <siddharthchandrakar007@gmail.com>
2024-04-14 10:32:40 +0530

    remove public from gallery

--- src/Components/Description/Gallery.jsx
+++ src/Components/Description/Gallery.jsx
@@ -8,33 +8,21 @@ function Gallery() {
       <h1 className="gallary-title">Gallery</h1>
       <Carousel data-bs-theme="dark">
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/public/wave.png"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src="wave.png" alt="First slide" />
           <Carousel.Caption>
             <h5>First slide label</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/public/wave.png"
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src="wave.png" alt="Second slide" />
           <Carousel.Caption>
             <h5>Second slide label</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="/public/wave.png"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src="wave.png" alt="Third slide" />
           <Carousel.Caption>
             <h5>Third slide label</h5>
             <p>
