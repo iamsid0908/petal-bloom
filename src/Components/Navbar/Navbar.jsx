72d1009638f678b0f44a20656f54bf95eb2ce5a2
iamsid0908 <siddharthchandrakar007@gmail.com>
2024-08-16 20:53:58 +0530

    fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,7 +17,10 @@ function Navbar() {
           <Link style={{ textDecoration: "none" }} to="/about">
             <a className="home">About us</a>
           </Link>
-          <a className="home">Addmission</a>
+          <Link style={{ textDecoration: "none" }} to="/admin">
+            <a className="home">Addmissioan</a>
+          </Link>
+
           <Link style={{ textDecoration: "none" }} to="/contact-us">
             <a className="home">Contact us</a>
           </Link>
